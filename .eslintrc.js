a5f051fdaf40a0b50bf6eeb85b95c2f897140872
natsuozawa <natsuozawa@outlook.com>
2018-08-25 21:46:34 +0900

    changed eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,45 +1,46 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
+  "env": {
+    "browser": true,
+    //to enable browser APIs that are supported by React, like fetch
+    "commonjs": true,
+    "es6": true
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
     },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "react-native"
+    "ecmaVersion": 2018,
+    "sourceType": "module"
+  },
+  "plugins": [
+    "react"
+  ],
+  "rules": {
+    /* for standardization */
+    "indent": [
+      "warn",
+      2
     ],
-    "rules": {
-        /* for standardization */
-        "indent": [
-            "warn",
-            2
-        ],
-        "linebreak-style": [
-            "warn",
-            "windows"
-        ],
-        "quotes": [
-            "warn",
-            "single"
-        ],
-        "semi": [
-            "warn",
-            "always"
-        ],
-        "no-unused-vars": [
-            "off"
-            /* DO NOT CHANGE -- turned off as no-unused-vars does not recognize component reference in JSX */       
-        ],
-        "no-console": [
-            "off"
-            /* We are not in production mode */
-        ]
-    }
+    "linebreak-style": [
+      "warn",
+      "windows"
+    ],
+    "quotes": [
+      "warn",
+      "single"
+    ],
+    "semi": [
+      "warn",
+      "always"
+    ],
+    "no-unused-vars": [
+      "off"
+      /* DO NOT CHANGE -- turned off as no-unused-vars does not recognize component reference in JSX */       
+    ],
+    "no-console": [
+      "off"
+      /* We are not in production mode */
+    ]
+  }
 };
