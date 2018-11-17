ef2723f7bc1c3a4bc2733872e00a685553c48efa
FoxInFlame <burningfoxinflame@gmail.com>
2018-11-17 19:41:35 +0900

    Change to more comprehensive ES style

--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -10,7 +10,7 @@ export default class ChatScreen extends React.Component {
     super(props);
   }
 
-  static navigationOptions({ navigation }) {
+  static navigationOptions = ({ navigation }) => {
     return {
       title: 'Chat'
     };
