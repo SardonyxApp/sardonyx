d46e4dbb580c33c0cac2b18fc807137a7fae6610
Yuto Takano <moa17stock@gmail.com>
2018-11-17 19:41:53 +0900

    Add Settings and HeaderIcon component for touchables

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -4,16 +4,30 @@ import {
   View
 } from 'react-native';
 
+import { Icon } from 'react-native-elements';
+import HeaderIcon from '../components/HeaderIcon';
 
 export default class ProfileScreen extends React.Component {
   constructor(props) {
     super(props);
   }
 
-  static navigationOptions({ navigation }) {
+  static navigationOptions = ({ navigation }) => {
     return {
-      title: 'Profile'
-    };
+      headerTitle: 'Profile',
+      headerStyle: {
+        backgroundColor: '#fff'
+      },
+      headerTintColor: '#d17b46',
+      headerTitleStyle: {
+        fontWeight: 'normal'
+      },
+      headerRight: (
+      <HeaderIcon onPress={() => {navigation.navigate('Settings')}}><Icon
+        name="settings"
+        color={"#d17b46"}
+        /></HeaderIcon>)
+    }
   }
 
   render() {
