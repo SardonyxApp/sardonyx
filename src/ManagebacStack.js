cf5fb6af7432c610a7e0c5b264dafdc5fdde916e
Yuto Takano <moa17stock@gmail.com>
2018-12-20 18:55:23 +0900

    Update react-navigation to 3.0 and add a TabNaivgator inside ManagebacStack

--- src/ManagebacStack.js
+++ src/ManagebacStack.js
@@ -1,24 +1,62 @@
 import React from 'react';
 
-import { createStackNavigator } from 'react-navigation';
-
 import {
-  Icon
-} from 'react-native-elements';
+  createStackNavigator,
+  createMaterialTopTabNavigator
+} from 'react-navigation';
 
-import ManagebacScreen from './screens/ManagebacScreen';
+import { Icon } from 'react-native-elements';
+
+import ManagebacOverviewScreen from './screens/ManagebacOverviewScreen';
+// Dummy screens
+import ManagebacCalendarScreen from './screens/ManagebacScreen';
+import ManagebacClassesScreen from './screens/ManagebacScreen';
+import ManagebacGroupsScreen from './screens/ManagebacScreen';
+import ManagebacCASScreen from './screens/ManagebacScreen';
+import MessagesScreen from './screens/SettingsScreen';
 
 import { colors } from './styles';
 
-// Navigation stack for the Managebac tab
-const ManagebacStack = createStackNavigator(
+const ManagebacTabs = createMaterialTopTabNavigator(
   {
-    Managebac: ManagebacScreen
+    Overview: ManagebacOverviewScreen,
+    Calendar: ManagebacCalendarScreen,
+    Groups: ManagebacClassesScreen,
+    Groups: ManagebacGroupsScreen,
+    CAS: ManagebacCASScreen
   },
   {
     navigationOptions: {
+      title: 'ManageBac'
+    },
+    tabBarOptions: {
+      scrollEnabled: true,
+      style: {
+        backgroundColor: colors.blue
+      },
+      indicatorStyle: {
+        backgroundColor: colors.white
+      }
+    }
+  }
+);
+// Navigation stack for the Managebac tab
+const ManagebacStack = createStackNavigator(
+  {
+    Managebac: ManagebacTabs,
+    Messages: MessagesScreen
+  },
+  {
+    navigationOptions: {
+      tabBarLabel: 'ManageBac',
+      tabBarIcon: ({ tintColor }) => (
+        <Icon name="globe" type={'feather'} color={tintColor} />
+      )
+    },
+    defaultNavigationOptions: {
       headerStyle: {
         backgroundColor: colors.blue,
+        elevation: 0
       },
       headerTintColor: colors.white,
       headerTitleStyle: {
@@ -27,14 +65,5 @@ const ManagebacStack = createStackNavigator(
     }
   }
 );
-
-// Applied after definition to prevent it from affecting children
-ManagebacStack.navigationOptions = {
-  tabBarLabel: 'Managebac',
-  tabBarIcon: ({ tintColor }) => (
-    <Icon name="globe" type={"feather"} color={tintColor} />
-  ),
-  tabBarColor: colors.white
-};
 
 export default ManagebacStack;
