5278a13aa93fe87b3aebb4f87c17e93b8db1a99f
Yuto Takano <moa17stock@gmail.com>
2018-12-21 17:46:45 +0900

    Add Classes tab and in Overview check that Storage is accessible

--- src/screens/ManagebacOverviewScreen.js
+++ src/screens/ManagebacOverviewScreen.js
@@ -2,15 +2,27 @@ import React from 'react';
 
 import { View } from 'react-native';
 
+import { Storage } from '../helpers';
+
 export default class ManagebacOverviewScreen extends React.Component {
   constructor(props) {
     super(props);
   }
 
-  static navigationOptions = ({ navigation }) => {
+  static navigationOptions({ navigation }) {
     return {
       title: 'Overview'
     };
+  };
+
+  componentDidMount() {
+    this._getOverviewData();
+  }
+  
+  _getOverviewData() {
+    Storage.retrieveValue('managebacOverview').then((data) => {
+      console.log(data);
+    });
   };
 
   render() {
