b738ff930ad7bf1d16384edebf67c178021475a0
Yuto Takano <moa17stock@gmail.com>
2018-12-24 21:11:00 +0900

    Add coloured labels, default loading data, and changed avatar positions

--- src/screens/ManagebacOverviewScreen.js
+++ src/screens/ManagebacOverviewScreen.js
@@ -17,8 +17,26 @@ export default class ManagebacOverviewScreen extends React.Component {
     super(props);
 
     this.state = {
-      deadlines: []
+      deadlines: [
+        {
+          'author': '',
+          'avatar': 'https://managebac-production.s3.amazonaws.com/uploads/school/logo/10004416/school_logo.png',
+          'deadline': false,
+          'due': '2050-01-01T00:00:00.000Z',
+          'labels': [],
+          'link': 'Loading',
+          'title': 'Loading'
+        }
+      ],
+      allGroupsClasses: [
+        {
+          'link': 'Loading',
+          'title': 'Please wait just a moment...'
+        }
+      ]
     };
+
+    this._getGroupClassName = this._getGroupClassName.bind(this);
   }
 
   static navigationOptions({ navigation }) {
@@ -28,24 +46,11 @@ export default class ManagebacOverviewScreen extends React.Component {
   }
 
   componentDidMount() {
-    let tmp_data = [
-      {
-        author: 'Yuto Takano',
-        avatar: 'https://avatars1.githubusercontent.com/u/4458848?s=52&v=4',
-        deadline: false,
-        due: '2018-12-31T20:00:00.000Z',
-        labels: ['Summative', 'Formative'],
-        link: '/student/classes/10901407/assignments/a',
-        title: 'Test title goes here. It can be extremely long as well.'
-      }
-    ];
-
     this._getOverviewData().then(data => {
       console.log(data);
-      console.log(typeof data);
-      console.log(data.deadlines);
       this.setState({
-        deadlines: [...data.deadlines, ...tmp_data]
+        deadlines: [...data.deadlines],
+        allGroupsClasses: [...data.groups, ...data.classes]
       });
     });
   }
@@ -58,31 +63,103 @@ export default class ManagebacOverviewScreen extends React.Component {
     });
   }
 
+  _renderLabel(name, index) {
+    let color;
+    switch (name) {
+      case 'Homework':
+        color = '#2175c6';
+        break;
+      case 'Quiz':
+        color = '#f16522';
+        break;
+      case 'Essay':
+        color = '#3333cc';
+        break;
+      case 'Deadline':
+        color = '#91181b';
+        break;
+      case 'Math IA':
+        color = '#0072bc';
+        break;
+      case 'Assignment':
+        color = '#0072bc';
+        break;
+      case 'Event':
+        color = '#009900';
+        break;
+      case 'Workshop':
+        color = '#528c00';
+        break;
+      case 'Take Home Assignment':
+        color = '#2f3192';
+        break;
+      case 'Discussion':
+        color = '#ed008c';
+        break;
+      case 'ToK':
+        color = '#3333cc';
+        break;
+      case 'Paper':
+        color = '#a2c400';
+        break;
+      case 'Summative':
+        color = '#478cfe';
+        break;
+      case 'Formative':
+        color = '#1aaf5d';
+        break;
+      case 'Extended Essay':
+        color = '#cc3333';
+        break;
+      default:
+        return;
+    }
+    return (
+      <View
+        key={index}
+        style={{ ...deadlineListStyles.label, backgroundColor: color }}
+      />
+    );
+  }
+
+  _getGroupClassName(link) {
+    let groupClass = this.state.allGroupsClasses.find(item => {
+      return link.includes(item.link);
+    });
+    return groupClass ? decodeURI(groupClass.title) : '';
+  }
+
   render() {
+    let i = 0;
+
     return (
       <FlatList
-        style={deadlineListStyles.list}
+        contentContainerStyle={deadlineListStyles.list}
         data={this.state.deadlines}
         renderItem={({ item }) => (
           <View style={deadlineListStyles.item}>
             <View style={deadlineListStyles.container}>
               <View style={deadlineListStyles.labels}>
-                <View style={deadlineListStyles.label} />
+                {item.labels.map(this._renderLabel)}
               </View>
               <View style={deadlineListStyles.bodyView}>
                 <View style={deadlineListStyles.text}>
                   <Text style={deadlineListStyles.title}>
                     {decodeURI(item.title)}
                   </Text>
-                  <Text style={deadlineListStyles.subject}>Niology</Text>
+                  <Text style={deadlineListStyles.subject}>
+                    {this._getGroupClassName(item.link)}
+                  </Text>
                 </View>
                 <View style={deadlineListStyles.bottomInfo}>
                   <Text style={deadlineListStyles.dueTime}>{item.due}</Text>
                   <View style={deadlineListStyles.avatars}>
-                    <Image
-                      style={deadlineListStyles.avatar}
-                      source={{ uri: item.avatar }}
-                    />
+                    <View style={deadlineListStyles.avatarContainer}>
+                      <Image
+                        style={deadlineListStyles.avatar}
+                        source={{ uri: item.avatar }}
+                      />
+                    </View>
                   </View>
                 </View>
               </View>
@@ -97,7 +174,8 @@ export default class ManagebacOverviewScreen extends React.Component {
 
 const deadlineListStyles = StyleSheet.create({
   list: {
-    backgroundColor: '#f8f8fa'
+    backgroundColor: '#f8f8fa',
+    paddingBottom: 24
   },
   item: {
     marginTop: 12,
@@ -114,21 +192,22 @@ const deadlineListStyles = StyleSheet.create({
   },
   label: {
     marginLeft: 6, // ((width:36+12) - 12) / 2 - 12
+    marginBottom: 6,
     borderRadius: 6,
     height: 12,
-    width: 12,
-    backgroundColor: '#00aa00'
+    width: 12
   },
   bodyView: {
     flex: 1,
+    flexDirection: 'column',
     borderRadius: 8,
     backgroundColor: colors.white,
     minHeight: 50,
-    elevation: 2,
+    elevation: 3,
     padding: 24
   },
   text: {
-    marginBottom: 24
+    marginBottom: 6
   },
   title: {
     fontWeight: 'bold',
@@ -138,6 +217,30 @@ const deadlineListStyles = StyleSheet.create({
   },
   subject: {
     fontSize: 14,
-    color: colors.darkBackground
+    color: colors.darkBackground,
+    fontWeight: '200'
+  },
+  bottomInfo: {
+    marginTop: 6,
+    flex: 1,
+    flexDirection: 'row'
+  },
+  dueTime: {
+    textAlignVertical: 'center'
+  },
+  avatars: {
+    flex: 1,
+    alignItems: 'flex-end',
+    height: 36
+  },
+  avatarContainer: {
+    width: 36,
+    height: 36,
+    borderRadius: 18,
+    overflow: 'hidden',
+    elevation: 2
+  },
+  avatar: {
+    flex: 1
   }
 });
