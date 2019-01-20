485c6bd96791b8554cf4ce8b556c0b2be961a81b
FoxInFlame <burningfoxinflame@gmail.com>
2019-01-20 11:30:30 +0900

    Add a screen under ManagebacStack for CAS

---
+++ src/screens/ManagebacCASScreen.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { View, Text } from 'react-native';
+
+import { Button } from 'react-native-elements';
+
+import { styles } from '../styles';
+
+export default class ManagebacCASScreen extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: 'CAS'
+    };
+  };
+
+  render() {
+    return (
+      <View style={styles.containerAlignChildrenCenter}>
+        <Text style={styles.h1}>This is the app!!</Text>
+        <Text style={styles.p}>
+          Check console to see your stored credentials.
+        </Text>
+      </View>
+    );
+  }
+}
