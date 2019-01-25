182afee7d07fe7a8ac79bff6048d6353f5965354
FoxInFlame <burningfoxinflame@gmail.com>
2019-01-25 16:19:13 +0900

    Start implementing CAS - Currently will only show links for each item

--- src/screens/ManagebacCASScreen.js
+++ src/screens/ManagebacCASScreen.js
@@ -1,14 +1,22 @@
 import React from 'react';
 
-import { View, Text } from 'react-native';
+import { View, Text, FlatList, StyleSheet, RefreshControl } from 'react-native';
 
-import { Button } from 'react-native-elements';
+import { BASE_URL } from 'react-native-dotenv';
 
+import { Storage } from '../helpers';
 import { styles } from '../styles';
 
 export default class ManagebacCASScreen extends React.Component {
   constructor(props) {
     super(props);
+    this.componentOnFocus = this.componentOnFocus.bind(this);
+    this._onRefresh = this._onRefresh.bind(this);
+    this.props.navigation.addListener('didFocus', this.componentOnFocus);
+    this.state = {
+      refreshing: true,
+      casExperiences: []
+    };
   }
 
   static navigationOptions = ({ navigation }) => {
@@ -17,14 +25,54 @@ export default class ManagebacCASScreen extends React.Component {
     };
   };
 
+  componentOnFocus(payload) {
+    if (this.state.casExperiences.length !== 0) return;
+    this._onRefresh();
+  }
+
+  _onRefresh() {
+    this.setState({
+      refreshing: true
+    });
+    Storage.retrieveCredentials().then(credentials => {
+      fetch(BASE_URL + '/api/cas', {
+        method: 'GET',
+        headers: {
+          'Login-Token': credentials
+        },
+        mode: 'no-cors'
+      }).then(response => {
+        if (response.status === 200) {
+          this.setState({
+            refreshing: false,
+            casExperiences: JSON.parse(response.headers.map['managebac-data']).cas
+          });
+          return;
+        }
+      });
+    });
+  }
+
   render() {
     return (
-      <View style={styles.containerAlignChildrenCenter}>
-        <Text style={styles.h1}>This is the app!!</Text>
-        <Text style={styles.p}>
-          Check console to see your stored credentials.
-        </Text>
-      </View>
+      <FlatList
+        contentContainerStyle={[CASListStyles.list, styles.lightBackground]}
+        refreshControl={(
+          <RefreshControl
+            refreshing={this.state.refreshing}
+            onRefresh={this._onRefresh}
+          />
+        )}
+        data={this.state.casExperiences}
+        renderItem={({ item }) => <Text>{item.link}</Text>}
+      />
     );
   }
 }
+
+const CASListStyles = StyleSheet.create({
+  list: {
+    paddingBottom: 24,
+    minHeight: '100%'
+  }
+});
