747550b3546c37c743350af5377985e5d276e7fb
Yuto Takano <moa17stock@gmail.com>
2019-03-16 03:12:02 +0900

    Implement ClassList, and the structure for ClassScreen

--- src/screens/ManagebacClassScreen.js
+++ src/screens/ManagebacClassScreen.js
@@ -1,19 +1,42 @@
 import React from 'react';
 
-import { View } from 'react-native';
+import { ScrollView } from 'react-native';
 
 export default class ManagebacClassScreen extends React.Component {
+  isMounted = false;
+
   constructor(props) {
     super(props);
+    this.state = {
+      refreshing: true,
+      classOverviewData: {},
+      classAssignmentsData: {},
+      classMessagesData: {}
+    };
+    this._onRefresh = this._onRefresh.bind(this);
+  }
+
+  componentDidMount() {
+    this._isMounted = true;
+    this._onRefresh();
+  }
+
+  componentWillUnmount() {
+    this._isMounted = false;
   }
 
   static navigationOptions = ({ navigation }) => {
     return {
-      title: 'Class'
+      title: `${navigation.state.params.title}`
     };
   };
 
+  /**
+   * Called on load, and on pull-to-refresh. Asynchronously sets the state using newest class data.
+   */
+  _onRefresh() {}
+
   render() {
-    return <View />;
+    return <ScrollView />;
   }
 }
