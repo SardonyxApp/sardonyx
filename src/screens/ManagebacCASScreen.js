b8bf20dfdffb29e5385d122c162215039e12fd32
Yuto Takano <moa17stock@gmail.com>
2019-03-16 02:45:21 +0900

    Basic CAS screen complete

--- src/screens/ManagebacCASScreen.js
+++ src/screens/ManagebacCASScreen.js
@@ -1,23 +1,40 @@
 import React from 'react';
 
-import { View, Text, FlatList, StyleSheet, RefreshControl, Alert } from 'react-native';
+import {
+  ScrollView,
+  View,
+  Text,
+  FlatList,
+  StyleSheet,
+  RefreshControl,
+  Alert
+} from 'react-native';
 
 import { BASE_URL } from 'react-native-dotenv';
 
 import { Storage } from '../helpers';
-import { styles } from '../styles';
+import { styles, fonts, colors } from '../styles';
+import Timespan from '../components/Timespan';
 
 export default class ManagebacCASScreen extends React.Component {
+  isMounted = false;
+
   constructor(props) {
     super(props);
     this.state = {
       refreshing: true,
+      casExperienceData: {}
     };
     this._onRefresh = this._onRefresh.bind(this);
   }
 
   componentDidMount() {
+    this._isMounted = true;
     this._onRefresh();
+  }
+
+  componentWillUnmount() {
+    this._isMounted = false;
   }
 
   static navigationOptions = ({ navigation }) => {
@@ -26,35 +43,96 @@ export default class ManagebacCASScreen extends React.Component {
     };
   };
 
+  /**
+   * Called on load, and on pull-to-refresh. Asynchronously sets the state using newest experience data.
+   */
   _onRefresh() {
-    this.setState({
-      refreshing: true
-    }, () => {
-      Storage.retrieveCredentials().then(credentials => {
-        fetch(BASE_URL + this.props.navigation.getParam('apiLink', '/404'), {
-          method: 'GET',
-          headers: {
-            'Login-Token': credentials
-          },
-          mode: 'no-cors'
-        }).then(response => {
-          if (response.status === 200) {
-            this.setState({
-              refreshing: false,
-              experienceData: JSON.parse(response.headers.map['managebac-data']).cas
-            });
-            return;
-          } else if (response.status === 404) {
-            Alert.alert('Not Found', 'Your CAS experience could not be found.', []);
-            this.props.navigation.goBack();
-            return;
-          }
+    this.setState(
+      {
+        refreshing: true
+      },
+      () => {
+        Storage.retrieveCredentials().then(credentials => {
+          fetch(BASE_URL + this.props.navigation.getParam('apiLink', '/404'), {
+            method: 'GET',
+            headers: {
+              'Login-Token': credentials
+            },
+            mode: 'no-cors'
+          }).then(response => {
+            if (!this._isMounted) return;
+            if (response.status === 200) {
+              this.setState({
+                refreshing: false,
+                casExperienceData: JSON.parse(
+                  response.headers.map['managebac-data']
+                ).cas
+              });
+              return;
+            } else if (response.status === 404) {
+              Alert.alert(
+                'Not Found',
+                'Your CAS experience could not be found.',
+                []
+              );
+              this.props.navigation.goBack();
+              return;
+            }
+          });
         });
-      });
-    });
+      }
+    );
   }
 
   render() {
-    return (null);
+    return (
+      <ScrollView
+        refreshControl={
+          <RefreshControl
+            refreshing={this.state.refreshing}
+            onRefresh={this._onRefresh}
+          />
+        }
+      >
+        <Timespan
+          timespan={
+            'timespan' in this.state.casExperienceData
+              ? this.state.casExperienceData.timespan
+              : null
+          }
+        />
+        {'description' in this.state.casExperienceData ? (
+          <View>
+            <View style={casStyles.detailsContainer}>
+              <Text style={casStyles.detailsHeading}>Description</Text>
+              <Text>{decodeURI(this.state.casExperienceData.description)}</Text>
+            </View>
+            <View style={casStyles.detailsSeparator} />
+            <View style={casStyles.detailsContainer}>
+              <Text style={casStyles.detailsHeading}>Meeting Learning Outcomes</Text>
+              <Text>{decodeURI(this.state.casExperienceData.learningOutcomes)}</Text>
+            </View>
+          </View>)
+          : null
+        }
+      </ScrollView>
+    );
   }
 }
+
+const casStyles = StyleSheet.create({
+  detailsContainer: {
+    marginHorizontal: 16
+  },
+  detailsHeading: {
+    fontSize: 16,
+    paddingTop: 4,
+    paddingBottom: 4,
+    ...fonts.jost500
+  },
+  detailsSeparator: {
+    marginVertical: 12,
+    height: 1,
+    backgroundColor: colors.gray1,
+  }
+});
