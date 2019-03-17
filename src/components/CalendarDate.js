fe492e17c8f314aa5d67da8804f8b712e7e77948
Yuto Takano <moa17stock@gmail.com>
2019-03-17 23:37:22 +0900

    Revert back to .getMonth() because Intl is not available in RN

--- src/components/CalendarDate.js
+++ src/components/CalendarDate.js
@@ -8,14 +8,29 @@ export default class CalendarDate extends React.PureComponent {
   constructor(props) {
     super(props);
   }
-  
+
+  shortMonthNames = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec'
+  ];
+
   render() {
     return (
       <View style={calendarStyles.container}>
         <View style={calendarStyles.calendar}>
           <View style={calendarStyles.month}>
             <Text style={calendarStyles.monthText}>
-              {new Intl.DateTimeFormat('en-US', { month: 'short'}).format(this.props.date)}
+              {this.shortMonthNames[this.props.date.getMonth()]}
             </Text>
           </View>
           <View style={calendarStyles.date}>
