882d537938c4e2a5b0568aa5294f829b554c31a3
Yuto Takano <moa17stock@gmail.com>
2019-03-30 20:09:50 +0900

    Remove agreement checkbox and add styles to textbox

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -10,10 +10,7 @@ import {
   Keyboard
 } from 'react-native';
 
-import {
-  CheckBox,
-  Button
-} from 'react-native-elements';
+import { Button } from 'react-native-elements';
 
 import { BASE_URL } from '../../env';
 
@@ -24,13 +21,24 @@ export default class Login extends React.Component {
   render() {
     return (
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <KeyboardAvoidingView style={[styles.alignChildrenCenter, styles.fullScreen]}>
+        <KeyboardAvoidingView
+          style={[styles.alignChildrenCenter, styles.fullScreen]}
+        >
           <View style={preset.loginBox}>
-            <Image source={require('../logos/Icon.png')} style={styles.logoIcon} />
-            <Text style={[styles.h1, styles.alignCenter, fonts.jost300]}>Sardonyx</Text>
-            <Text style={[styles.p, styles.alignCenter, fonts.jost400]}>Login with ManageBac</Text>
+            <Image
+              source={require('../logos/Icon.png')}
+              style={styles.logoIcon}
+            />
+            <Text style={[styles.h1, styles.alignCenter, fonts.jost300]}>
+              Sardonyx
+            </Text>
+            <Text style={[styles.p, styles.alignCenter, fonts.jost400]}>
+              Login with ManageBac
+            </Text>
             <LoginForm navigation={this.props.navigation} />
-            <ErrorMessage error={this.props.navigation.getParam('errorMessage', null)} />
+            <ErrorMessage
+              error={this.props.navigation.getParam('errorMessage', null)}
+            />
           </View>
           <DisclaimerMessage />
         </KeyboardAvoidingView>
@@ -48,7 +56,6 @@ class LoginForm extends React.Component {
       agree: false,
       usernameError: false,
       passwordError: false,
-      agreeError: false,
       disabled: false
     };
 
@@ -84,77 +91,108 @@ class LoginForm extends React.Component {
     // returning a promise because setState does not get immediately reflected
     return new Promise(resolve => {
       //raise error if username is not a valid email address, or if one of the fields are empty
-      this.setState({
-        usernameError: !emailRegex.test(this.state.username),
-        passwordError: this.state.password.length < 1,
-        agreeError: !this.state.agree
-      }, () => {
-        // if there is either error, return false to reject request
-        resolve(this.state.usernameError || this.state.passwordError || this.state.agreeError);
-      });
+      this.setState(
+        {
+          usernameError: !emailRegex.test(this.state.username),
+          passwordError: this.state.password.length < 1
+        },
+        () => {
+          // if there is either error, return false to reject request
+          resolve(this.state.usernameError || this.state.passwordError);
+        }
+      );
     });
   }
 
   sendForm(formData) {
-    fetch(BASE_URL + '/api/login', { 
+    fetch(BASE_URL + '/api/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'multipart/form-data'
       },
       body: formData,
-      mode: 'no-cors',
-    }).then(response => {
-      if (response.status === 200) {
-        // store response tokens
-        const credentials = JSON.parse(response.headers.map['login-token'] || '{}');
-        Storage.writeCredentials(credentials).then(() => {
-          this.toggleButton(); // Make button available again
-          this.props.navigation.navigate('AppStack');
-        }).catch(error => {
+      mode: 'no-cors'
+    })
+      .then(response => {
+        if (response.status === 200) {
+          // store response tokens
+          const credentials = JSON.parse(
+            response.headers.map['login-token'] || '{}'
+          );
+          Storage.writeCredentials(credentials)
+            .then(() => {
+              this.toggleButton(); // Make button available again
+              this.props.navigation.navigate('AppStack');
+            })
+            .catch(error => {
+              this.toggleButton();
+              this.props.navigation.navigate('Login', {
+                errorMessage: 'There was an error while storing login. ' + error
+              });
+            });
+        } else {
           this.toggleButton();
-          this.props.navigation.navigate('Login', {
-            errorMessage: 'There was an error while storing login. ' + error
-          });
-        });
-      } else {
+          if (response.status === 401)
+            this.props.navigation.navigate('Login', {
+              errorMessage:
+                'Your username and password did not match. Please retry.'
+            });
+          else if (response.status === 404)
+            this.props.navigation.navigate('Login', {
+              errorMessage: 'Validation failed due to a network error.'
+            });
+          else
+            this.props.navigation.navigate('Login', {
+              errorMessage:
+                'Validation failed due to an unknown error. Error code: ' +
+                response.status
+            });
+        }
+      })
+      .catch(error => {
         this.toggleButton();
-        if (response.status === 401) this.props.navigation.navigate('Login', {
-          errorMessage: 'Your username and password did not match. Please retry.'
+        this.props.navigation.navigate('Login', {
+          errorMessage:
+            'There was an error while processing your login. Please retry. ' +
+            error
         });
-        else if (response.status === 404) this.props.navigation.navigate('Login', {
-          errorMessage: 'Validation failed due to a network error.'
-        });
-        else this.props.navigation.navigate('Login', {
-          errorMessage: 'Validation failed due to an unknown error. Error code: ' + response.status
-        });
-      }
-    }).catch(error => {
-      this.toggleButton();
-      this.props.navigation.navigate('Login', {
-        errorMessage: 'There was an error while processing your login. Please retry. ' + error
       });
-    });
   }
 
   render() {
     return (
-      <View style={{ flexDirection: 'column' }}>
-
-        <TextInput 
+      <View style={{ flexDirection: 'column', width: '100%', marginTop: 16 }}>
+        <TextInput
           placeholder="Username"
           value={this.state.username}
-          style={preset.inputLine}
+          style={[
+            preset.inputLine,
+            {
+              borderBottomWidth: 0.5,
+              borderTopRightRadius: 4,
+              borderTopLeftRadius: 4
+            }
+          ]}
           textContentType="username"
           keyboardType="email-address"
           returnKeyType="next"
           autoCapitalize="none"
-          onChangeText={text => this.setState({
-            username: text
-          })}
+          onChangeText={text =>
+            this.setState({
+              username: text
+            })
+          }
           onSubmitEditing={() => this.passwordInput.focus()}
           blurOnSubmit={false}
         />
-        <Text style={[styles.error, styles.alignCenter, fonts.jost400, this.state.usernameError ? {} : styles.hidden]}>
+        <Text
+          style={[
+            styles.error,
+            styles.alignCenter,
+            fonts.jost400,
+            this.state.usernameError ? {} : styles.hidden
+          ]}
+        >
           Please enter a valid email address.
         </Text>
 
@@ -164,36 +202,34 @@ class LoginForm extends React.Component {
             this.passwordInput = input;
           }}
           value={this.state.password}
-          style={preset.inputLine}
+          style={[
+            preset.inputLine,
+            {
+              borderTopWidth: 0.5,
+              borderBottomRightRadius: 4,
+              borderBottomLeftRadius: 4
+            }
+          ]}
           textContentType="password"
           keyboardType="default"
           returnKeyType="done"
           autoCapitalize="none"
           secureTextEntry={true}
-          onChangeText={text => this.setState({
-            password: text 
-          })}
+          onChangeText={text =>
+            this.setState({
+              password: text
+            })
+          }
         />
-        <Text style={[styles.error, styles.alignCenter, fonts.jost400, this.state.passwordError ? {} : styles.hidden]}>
+        <Text
+          style={[
+            styles.error,
+            styles.alignCenter,
+            fonts.jost400,
+            this.state.passwordError ? {} : styles.hidden
+          ]}
+        >
           Please enter a password.
-        </Text>
-
-        <CheckBox 
-          title="I agree to the Terms of Service and Privacy Policy"
-          checked={this.state.agree}
-          checkedColor={colors.black}
-          uncheckedColor={colors.black}
-          iconType="material-community"
-          checkedIcon="checkbox-marked"
-          uncheckedIcon="checkbox-blank-outline"
-          containerStyle={[styles.transparentBackground, {paddingBottom: 0}]}
-          onPress={() => this.setState({
-            agree: !this.state.agree
-          })}
-          textstyle={/*[styles.regular, styles.p]*/ styles.link}
-        />
-        <Text style={[styles.error, styles.alignCenter, fonts.jost400, this.state.agreeError ? {} : styles.hidden]}>
-          Please agree to the Conditions.
         </Text>
 
         <Button
@@ -204,7 +240,6 @@ class LoginForm extends React.Component {
           disabled={this.state.disabled}
           disabledStyle={{ backgroundColor: colors.lightPrimary }}
         />
-        
       </View>
     );
   }
@@ -212,15 +247,41 @@ class LoginForm extends React.Component {
 
 function DisclaimerMessage() {
   return (
-    <Text style={[styles.small, styles.alignCenter, styles.padding5, fonts.jost300]}>
-      Sardonyx is not affiliated, associated, authorized, endorsed by, or in any way officially connected with ManageBac, or any of its subsidiaries or its affiliates.
-    </Text>
+    <View style={styles.alignCenter}>
+      <Text
+        style={[
+          styles.small,
+          styles.alignCenter,
+          styles.padding5,
+          fonts.jost300
+        ]}
+      >
+        By signing in, you are agreeing to our Terms of Service and Privacy
+        Policy.
+      </Text>
+      <Text
+        style={[
+          styles.small,
+          styles.alignCenter,
+          styles.padding5,
+          fonts.jost300
+        ]}
+      >
+        Sardonyx is not affiliated, associated, authorized, endorsed by, or in
+        any way officially connected with ManageBac, or any of its subsidiaries
+        or its affiliates.
+      </Text>
+    </View>
   );
 }
 
 function ErrorMessage(props) {
   if (props.error) {
-    return <Text style={[styles.p, styles.alignCenter, styles.error, fonts.jost400]}>{props.error}</Text>;
+    return (
+      <Text style={[styles.p, styles.alignCenter, styles.error, fonts.jost400]}>
+        {props.error}
+      </Text>
+    );
   }
   return null;
 }
