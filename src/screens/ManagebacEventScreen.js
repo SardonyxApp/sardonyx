49fcc91043706ecd3e4bba0b55589698f93e88c3
FoxInFlame <burningfoxinflame@gmail.com>
2019-04-29 00:47:16 +0900

    Change null coalescing operator to ||

--- src/screens/ManagebacEventScreen.js
+++ src/screens/ManagebacEventScreen.js
@@ -186,7 +186,7 @@ export default class ManagebacEventScreen extends React.Component {
           <HTMLView
             value={
               'details' in this.state.upcomingEventData
-                ? (this.state.upcomingEventData.details ?? 'No details provided.')
+                ? (this.state.upcomingEventData.details || 'No details provided.')
                 : '<p />'
             }
             stylesheet={htmlStyles}
