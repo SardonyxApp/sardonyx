84b344e97a650e1123ac0242144c11bfa6cbcfc9
FoxInFlame <burningfoxinflame@gmail.com>
2019-04-28 23:41:42 +0900

    Change Upcoming event list to a carousel

---
+++ src/components/UpcomingCarousel.js
@@ -0,0 +1,122 @@
+import React from 'react';
+
+import { View, Text, Dimensions, StyleSheet } from 'react-native';
+
+import Carousel from 'react-native-snap-carousel';
+import { TouchableRipple } from 'react-native-paper';
+
+import CalendarDate from '../components/CalendarDate';
+import { colors, fonts } from '../styles';
+
+export default class UpcomingCarousel extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this._renderItem = this._renderItem.bind(this);
+  }
+
+  /**
+   * Get the name of the class or group that the event is located in.
+   * @param {String} link
+   */
+  _getGroupClassName(link) {
+    let groupClass = this.props.allGroupsAndClasses.find(item => {
+      return link.includes(item.link.slice(0, -9)); // Remove the /overview bit
+    });
+    return groupClass ? decodeURI(groupClass.title) : 'IB Manager';
+  }
+
+  _navigateToUpcomingEventScreen(item) {
+    this.props.navigation.navigate('UpcomingEventItem', {
+      ...item,
+      title: decodeURI(item.title),
+      groupClassName: this._getGroupClassName(item.link)
+    });
+  }
+
+  _renderItem({ item, index }) {
+    return (
+      <View style={upcomingCarouselStyles.wrapper}>
+        <View style={upcomingCarouselStyles.containerWrapper}>
+          <TouchableRipple
+            onPress={() => this._navigateToUpcomingEventScreen(item)}
+            rippleColor="rgba(0, 0, 0, .16)"
+          >
+            <View style={upcomingCarouselStyles.container}>
+              <CalendarDate date={new Date(Date.parse(item.due))} />
+              <View style={upcomingCarouselStyles.textContainer}>
+                <Text
+                  style={upcomingCarouselStyles.title}
+                  ellipsizeMode={'tail'}
+                >
+                  {decodeURI(item.title)}
+                </Text>
+                <Text
+                  style={upcomingCarouselStyles.subject}
+                  numberOfLines={1}
+                  ellipsizeMode={'tail'}
+                >
+                  {this._getGroupClassName(item.link)}
+                </Text>
+              </View>
+            </View>
+          </TouchableRipple>
+        </View>
+      </View>
+    );
+  }
+
+  render() {
+    return (
+      <View style={upcomingCarouselStyles.carouselContainer}>
+        <Carousel
+          data={this.props.upcomingEvents}
+          renderItem={this._renderItem}
+          sliderWidth={Dimensions.get('window').width}
+          itemWidth={300}
+          activeSlideAlignment={'start'}
+          inactiveSlideScale={1}
+          inactiveSlideOpacity={1}
+        />
+      </View>
+    );
+  }
+}
+
+const upcomingCarouselStyles = StyleSheet.create({
+  carouselContainer: {
+    height: 150
+  },
+  wrapper: {
+    paddingHorizontal: 16,
+    paddingVertical: 25
+  },
+  containerWrapper: {
+    backgroundColor: colors.white,
+    height: 100,
+    borderRadius: 4,
+    elevation: 15 // Really increase this so the shadows appear blurry like iOS,
+  },
+  container: {
+    flexDirection: 'row'
+  },
+  textContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    marginVertical: 14,
+    marginRight: 14
+  },
+  title: {
+    ...fonts.jost400,
+    fontSize: 16,
+    flex: 1,
+    flexWrap: 'wrap',
+    flexShrink: 1
+  },
+  subject: {
+    ...fonts.jost400,
+    fontSize: 9,
+    flexShrink: 1,
+    bottom: 0
+  }
+});
