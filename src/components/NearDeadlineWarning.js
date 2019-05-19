6b799b1b9d082be7513e6babc7996fbe1ed44f0f
Yuto Takano <moa17stock@gmail.com>
2019-05-19 17:58:38 +0900

    Check if deadline in past, then change message

--- src/components/NearDeadlineWarning.js
+++ src/components/NearDeadlineWarning.js
@@ -10,7 +10,7 @@ export default class NearDeadlineWarning extends React.PureComponent {
   }
 
   /**
-   * Calculates the time difference from now to the due date and returns it in a legible manner.
+   * Calculates the time difference from now to the due date and checks if it is in the next 24 hours.
    * @param {Date} dueDate
    * @param {Date} now
    * @return {Boolean}
@@ -22,19 +22,36 @@ export default class NearDeadlineWarning extends React.PureComponent {
   }
 
   /**
+   * Calculates the time difference from now to the due date and checks if it is in the past.
+   * @param {Date} dueDate
+   * @param {Date} now
+   * @return {Boolean}
+   */
+  _isInPast(dueDate, now) {
+    let difference = dueDate - now;
+    if (difference < 0) return true;
+    return false;
+  }
+
+  /**
    * Calls this._isWithinNextDay, to return a boolean of whether to render the warning or not.
    * @param {Date} dueDate
    * @return {Boolean}
    */
   _decideToRender(dueDate) {
-    return this._isWithinNextDay(dueDate, Date.now());
+    if (!this._isWithinNextDay(dueDate, Date.now())) return 0;
+    if (this._isInPast(dueDate, Date.now())) return 2;
+    return 1;
   }
 
   render() {
-    return this._decideToRender(this.props.date) ? (
+    const renderCondition = this._decideToRender(this.props.date);
+    return renderCondition !== 0 ? (
       <View style={warningStyles.container}>
         <Text style={warningStyles.text}>
-          The deadline for this task is in less than a day!
+          {renderCondition === 1
+            ? 'The task/event is in less than a day!'
+            : 'This task/event is in the past.'}
         </Text>
       </View>
     ) : null;
