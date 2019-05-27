273d126965fec654ab6977c2845e7bd5df77ea23
natsuozawa <natsuozawa@outlook.com>
2019-05-27 22:51:07 +0900

    Add task deletion

--- src/components/TaskDelete.js
+++ src/components/TaskDelete.js
@@ -1,32 +1,50 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import { Icon } from 'react-native-elements';
 
 import { styles, fonts } from '../styles';
 
-export default TaskDelete = props => (
-  <View
-    style={{
-      flexDirection: 'row',
-      alignItems: 'center',
-      paddingVertical: 8
-    }}
-  >
-    <Icon 
-      name="delete"
-      type="material"
-      iconStyles={styles.icon}
-      color="#f44138"
-    />
-    <Text
-      style={{
-        ...fonts.jost300,
-        fontSize: 16,
-        paddingHorizontal: 8,
-        color: '#f44138'
-      }}
-    >
-      Delete this task
-    </Text>
-  </View>
-);
+export default class TaskDelete extends React.Component {
+  constructor(props) {
+    super(props);
+    this._handleDelete = this._handleDelete.bind(this);
+  }
+
+  _handleDelete() {
+    this.props.onDeleteTask(this.props.id);
+    this.props.navigation.goBack();
+  }
+
+  render() {
+    return (
+      <View
+        style={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          paddingVertical: 8
+        }}
+      >
+        <Icon 
+          name="delete"
+          type="material"
+          iconStyles={styles.icon}
+          color="#f44138"
+        />
+        <TouchableOpacity
+          onPress={this._handleDelete}
+        >
+          <Text
+            style={{
+              ...fonts.jost300,
+              fontSize: 16,
+              paddingHorizontal: 8,
+              color: '#f44138'
+            }}
+          >
+            Delete this task
+          </Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
