70845c544839e3f5a02530876ee29703103d75cc
Yuto Takano <moa17stock@gmail.com>
2019-06-28 20:05:48 +0900

    Change multiple components to PureComponents

--- src/components/EndOfList.js
+++ src/components/EndOfList.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { View, Image, StyleSheet, Dimensions } from 'react-native';
 
-export default class EndOfList extends React.Component {
+export default class EndOfList extends React.PureComponent {
   constructor(props) {
     super(props);
   }
