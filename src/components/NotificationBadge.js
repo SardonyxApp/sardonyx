ebccc9ab2509ce70b7bfc71a9c5c6ab7ccf804d9
FoxInFlame <burningfoxinflame@gmail.com>
2019-08-25 01:14:19 +0900

    Implement connected NotificationBadge and tabbar

---
+++ src/components/NotificationBadge.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import { StyleSheet } from 'react-native';
+
+import { Badge } from 'react-native-paper';
+import { connect } from 'react-redux';
+
+import { colors } from '../styles';
+
+class NotificationBadge extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      this.props.overview.notificationCount ? (
+        <Badge style={[notificationBadgeStyles.badgeIcon, this.props.style]} size={18}>
+          {this.props.overview.notificationCount}
+        </Badge>
+      ) : null
+    );
+  }
+}
+
+const notificationBadgeStyles = StyleSheet.create({
+  badgeIcon: {
+    position: 'absolute',
+    top: -5,
+    color: colors.white,
+    backgroundColor: colors.primary
+    
+  }
+});
+
+const mapStateToProps = state => {
+  const overview = state.managebac.overview;
+  return { overview };
+};
+
+export default connect(mapStateToProps)(NotificationBadge);
