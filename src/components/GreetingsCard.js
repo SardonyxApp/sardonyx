5f97f1c3c66d47163993f16465fb1e3bdfe034b1
FoxInFlame <burningfoxinflame@gmail.com>
2019-09-08 11:02:36 +0900

    Replace blue greeting card with white/primary

--- src/components/GreetingsCard.js
+++ src/components/GreetingsCard.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
-import { View, StyleSheet, Dimensions, Text } from 'react-native';
+import {
+  View,
+  StyleSheet,
+  Dimensions,
+  Text,
+  Image,
+  TouchableOpacity
+} from 'react-native';
 
 import Lottie from 'lottie-react-native';
 import { connect } from 'react-redux';
+
+import NotificationBadge from './NotificationBadge';
+import { Icon } from 'react-native-elements';
 
 import { colors, fonts } from '../styles';
 
@@ -13,12 +23,16 @@ class GreetingsCard extends React.PureComponent {
   }
 
   componentDidMount() {
-    if(this.props.showOverviewAnimation) {
+    if (this.props.showOverviewAnimation) {
       setTimeout(() => {
         this.animation.play();
       }, 100);
     }
   }
+
+  _getGreeting(time) {
+
+  } 
 
   _getFirstName(text) {
     if (typeof text === 'string') return text.split(' ')[0];
@@ -28,7 +42,7 @@ class GreetingsCard extends React.PureComponent {
   render() {
     return (
       <View style={greetingsCardStyles.container}>
-        {this.props.showOverviewAnimation ? <Lottie
+        {/* {this.props.showOverviewAnimation ? <Lottie
           style={{
             width: Dimensions.get('window').width,
             height: (Dimensions.get('window').width / 1080) * 300,
@@ -41,13 +55,40 @@ class GreetingsCard extends React.PureComponent {
           loop={true}
           autoPlay={this.props.showOverviewAnimation}
           source={require('../assets/overview.json')}
-        /> : null}
+        /> : null} */}
+        <Image
+          source={require('../assets/logos/school.png')}
+          style={{
+            width: Dimensions.get('window').width,
+            height: (Dimensions.get('window').width / 36) * 25,
+            position: 'absolute',
+            bottom: 0
+          }}
+        />
         <Text style={greetingsCardStyles.title} numberOfLines={1}>
-          Hello{' '}
-          <Text style={greetingsCardStyles.name}>
-            {this._getFirstName(this.props.name)}
-          </Text>
+          Good Morning,
         </Text>
+        <Text style={greetingsCardStyles.name}>
+          {this._getFirstName(this.props.name)}
+        </Text>
+        <TouchableOpacity
+          style={greetingsCardStyles.notificationIcon}
+          onPress={() => {
+            this.props.navigation.navigate('Alerts', {
+              refreshPage: this.props.navigation.state.params.refreshPage
+            });
+          }}
+        >
+          <Icon
+            name={
+              this.props.notificationCount
+                ? 'notifications-active'
+                : 'notifications-none'
+            }
+            color={colors.primary}
+          />
+          <NotificationBadge />
+        </TouchableOpacity>
       </View>
     );
   }
@@ -56,19 +97,28 @@ class GreetingsCard extends React.PureComponent {
 const greetingsCardStyles = StyleSheet.create({
   container: {
     width: Dimensions.get('window').width,
-    height: 200,
-    backgroundColor: colors.blue,
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    height: (Dimensions.get('window').width / 36) * 20,
+    backgroundColor: colors.white,
+    flex: 1
   },
   title: {
-    ...fonts.jost200,
-    fontSize: 40,
-    color: colors.white
+    marginTop: 28,
+    paddingHorizontal: 16,
+    ...fonts.jost400,
+    fontSize: 24,
+    color: colors.lightPrimary
   },
   name: {
-    ...fonts.jost500
+    marginTop: -12,
+    paddingHorizontal: 16,
+    ...fonts.jost500,
+    fontSize: 56,
+    color: colors.darkPrimary2
+  },
+  notificationIcon: {
+    position: 'absolute',
+    top: 16,
+    right: 8
   }
 });
 
